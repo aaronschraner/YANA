cf3bcdfd524dcaca61a552ffea03ca6c01494f95
aaronschraner <aaron@pnwcab.com>
2014-11-15 22:48:46 -0800

    Added comments

--- app/src/main/java/www/yanastudios/com/yana/HomeActivity.java
+++ app/src/main/java/www/yanastudios/com/yana/HomeActivity.java
@@ -1,20 +1,26 @@
 package www.yanastudios.com.yana
 
+//definitely needs some more importing.
 import android.widget.ListView;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class HomeActivity extends Activity
 {
+  //extends Activity because it is an Activity
   
-  ListView cardListView;
+  ListView cardListView; 
+  //listView for cards
   
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
-    cardListView = (ListView) findViewById(R.id.listview);
+    //boilerplate stuff
+    
+    cardListView = (ListView) findViewById(R.id.listview); 
+    //get a reference to the card listview
     
   }
 }
