210806d5b50cc42d5da91d781363fa5355f68f58
aaronschraner <aaron@pnwcab.com>
2014-11-15 22:59:28 -0800

    Added comments and test data for cards

--- app/src/main/java/www/yanastudios/com/yana/HomeActivity.java
+++ app/src/main/java/www/yanastudios/com/yana/HomeActivity.java
@@ -12,15 +12,33 @@ public class HomeActivity extends Activity
   ListView cardListView; 
   //listView for cards
   
+  List<CardInfo> cards = new ArrayList<CardInfo>();
+  //list of cards to display
+  
+  ArrayAdapter cardAdapter;
+  //adapter for cards
+  public void initializeCards()
+  {
+    cards.add(new CardInfo("Test 1"));
+    cards.add(new CardInfo("Test 2"));
+    cards.add(new CardInfo("Test 3"));
+    //plug in some test data for our cards
+  }
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
     //boilerplate stuff
-    
+    initializeCards();
     cardListView = (ListView) findViewById(R.id.listview); 
     //get a reference to the card listview
+    
+    cardAdapter = new CardAdapter(this,cards);
+    //actually initialize the card adapter
+    
+    cardListView.setAdapter(cardAdapter);
+    //attach the cardAdapter to the cardListView 
     
   }
 }
