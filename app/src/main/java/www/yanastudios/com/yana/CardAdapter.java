f007347eea03be45fb5924f046bd452b31863c71
aaronschraner <aaron@pnwcab.com>
2014-11-15 22:46:38 -0800

    Added comments

--- app/src/main/java/www/yanastudios/com/yana/CardAdapter.java
+++ app/src/main/java/www/yanastudios/com/yana/CardAdapter.java
@@ -1,20 +1,25 @@
+//Adapter to go from CardInfo (with card data) to Card View (with same data)
 public class CardAdapter extends ArrayAdapter<CardInfo>
 {
   public CardAdapter(Context context, List<CardInfo> objects)
   {
-    super(context,0,objects);
+    super(context,0,objects); //???
   }
+  
   @Override
-  public View getView(int position, View convertView, ViewGroup parent)
+  public View getView(int position, View convertView, ViewGroup parent) //used to populate card views
   {
-    if(convertView == null)
+    if(convertView == null) //if card view has not been initialized
     {
       LayoutInflater inflater=LayoutInflater.from(getContext());
       convertView=inflater.inflate(R.layout.list_item_card, parent, false);
+      //initialize it.
     }
-    CardInfo currentCard = getItem(position);
-    TextView textView = (TextView) convertView.findViewById(R.id.card_text_view);
-    textView.setText(currentCard.getText());
-    return convertView;
+    
+    CardInfo currentCard = getItem(position); //Get a CardInfo object that corresponds to this view's card
+    TextView textView = (TextView) convertView.findViewById(R.id.card_text_view); //get a reference to the card text view
+    textView.setText(currentCard.getText()); //set the card text view to that myText variable from CardInfo
+    return convertView; //return the adapted thingy
   }
+  
 }
