cf441b818c5e689951ecc39622ad1ce33bfc20b2
aaronschraner <aaron@pnwcab.com>
2014-11-15 22:17:35 -0800

    Create CardInfo.java

---
+++ app/src/main/java/www/yanastudios/com/yana/CardInfo.java
@@ -0,0 +1,16 @@
+public class CardInfo
+{
+  String myText;
+  public CardInfo(String myText)
+  {
+    this.myText=myText;
+  }
+  public String getText()
+  {
+    return myText;
+  }
+  public void setText(String myText)
+  {
+      this.myText=myText;
+  }
+}
