fd2f3df462df4d85614323487a11d511d494b3ae
aaronschraner <aaron@pnwcab.com>
2014-11-15 21:28:50 -0800

    Create HomeActivity.java

---
+++ app/src/main/java/www/yanastudios/com/yana/HomeActivity.java
@@ -0,0 +1,11 @@
+package www.yanastudios.com.yana
+
+public class HomeActivity extends Activity
+{
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+  }
+}
